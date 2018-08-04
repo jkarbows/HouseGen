d5c5fc8a80a6ad203d65edd3e98168b0c7bdb114
Jeremy <jkarbows@gmail.com>
2018-08-04 11:21:10 -0400

    moving naive growing tree to its own file

--- Assets/HouseGen/InstancePainter/Editor/InstancePainterEditor.cs
+++ Assets/HouseGen/InstancePainter/Editor/InstancePainterEditor.cs
@@ -20,10 +20,6 @@ namespace ProcGenKit.WorldBuilding
         List<Bounds> overlaps = new List<Bounds>();
         List<GameObject> overlappedGameObjects = new List<GameObject>();
 
-        IntVector2 size;
-        BaseCell[,] cells;
-        List<Room> rooms = new List<Room>();
-
         void OnEnable()
         {
             stamp = new GameObject("Stamp");
@@ -48,7 +44,8 @@ namespace ProcGenKit.WorldBuilding
             while (stamp.transform.childCount > 0)
                 DestroyImmediate(stamp.transform.GetChild(0).gameObject);
 
-            Generate();
+            GrowingTree growingTree = ScriptableObject.CreateInstance<GrowingTree>();
+            growingTree.Generate(ip, stamp);
 
             var count = Mathf.Min(1000, (Mathf.PI * Mathf.Pow(ip.brushRadius, 2)) / (1f / ip.roomDensity));
 
@@ -148,160 +145,6 @@ namespace ProcGenKit.WorldBuilding
             {
                 t.localPosition = rotation * t.localPosition;
             }
-        }
-
-        void Generate()
-        {
-            // todo: repeat for each floor, 2x2
-            // each cell is 4x4
-            size.x = ip.brushRadius / 4;
-            size.z = ip.brushRadius / 4;
-            cells = new BaseCell[size.x, size.z];
-            List<BaseCell> activeCells = new List<BaseCell>();
-            IntVector2 coordinates = RandomCoordinates;
-            // do the first generation step so we have something to iterate from
-            PerformFirstGenerationStep(activeCells);
-            while (activeCells.Count > 0)
-            {
-                PerformNextGenerationStep(activeCells);
-            }
-        }
-
-        private void PerformFirstGenerationStep (List<BaseCell> activeCells)
-        {
-            BaseCell newCell = CreateCell(RandomCoordinates);
-            newCell.Initialize(CreateRoom(-1));
-            activeCells.Add(newCell);
-        }
-
-        private void PerformNextGenerationStep (List<BaseCell> activeCells)
-        {
-            int index = activeCells.Count - 1;
-            BaseCell currentCell = activeCells[index];
-            if (currentCell.IsFullyInitialized)
-            {
-                activeCells.RemoveAt(index);
-                return;
-            }
-            CompassDirection direction = currentCell.RandomUninitializedDirection;
-            IntVector2 coordinates = currentCell.coordinates + direction.ToIntVector2();
-            if (ContainsCoordinates(coordinates))
-            {
-                BaseCell neighbor = GetCell(coordinates);
-                if (neighbor == null)
-                {
-                    neighbor = CreateCell(coordinates);
-                    CreatePassage(currentCell, neighbor, direction);
-                    activeCells.Add(neighbor);
-                }
-                // if we match on room.settingsIndex instead we can merge same rooms
-                else if (currentCell.room == neighbor.room)
-                {
-                    CreatePassageInSameRoom(currentCell, neighbor, direction);
-                }
-                else
-                {
-                    CreateWall(currentCell, neighbor, direction);
-                }
-            }
-            else
-            {
-                CreateWall(currentCell, null, direction);
-            }
-        }
-
-        private BaseCell CreateCell(IntVector2 coordinates)
-        {
-            var child = new GameObject("Dummy");
-            child.transform.parent = stamp.transform;
-            child.transform.localPosition = new Vector3(coordinates.x * 4 - size.x * 4 * 0.5f + 0.5f, 0f, coordinates.z * 4 - size.z * 4 * 0.5f + 0.5f);
-            child.transform.localEulerAngles = Vector3.zero;
-            BaseCell cell = PrefabUtility.InstantiatePrefab(ip.baseCell) as BaseCell;
-            foreach (var c in cell.GetComponentsInChildren<Collider>())
-                c.enabled = false;
-            cells[coordinates.x, coordinates.z] = cell;
-            cell.coordinates = coordinates;
-            cell.transform.parent = child.transform;
-            cell.transform.localPosition = Vector3.zero;
-            cell.transform.localRotation = Quaternion.identity;
-            // originally set the parent to the maze's transform
-            // then set localposition to the new vector3
-            // instead of any of the child stuff
-            return cell;
-        }
-
-        private Room CreateRoom (int indexToExclude)
-        {
-            Room newRoom = ScriptableObject.CreateInstance<Room>();
-            newRoom.settingsIndex = Random.Range(0, ip.roomSettings.Length);
-            if (newRoom.settingsIndex == indexToExclude)
-            {
-                newRoom.settingsIndex = (newRoom.settingsIndex + 1) % ip.roomSettings.Length;
-            }
-            newRoom.settings = ip.roomSettings[newRoom.settingsIndex];
-            rooms.Add(newRoom);
-            return newRoom;
-        }
-
-        private void CreatePassage (BaseCell cell, BaseCell otherCell, CompassDirection direction)
-        {
-            CellPassage prefab = Random.value < ip.doorProbability ? ip.doorPrefab : ip.passagePrefab;
-            CellPassage passage = Instantiate(prefab) as CellPassage;
-            passage.Initialize(cell, otherCell, direction);
-            passage = Instantiate(prefab) as CellPassage;
-            if (passage is Door)
-            {
-                otherCell.Initialize(CreateRoom(-1 /*cell.room.settingsIndex*/));
-            }
-            else
-            {
-                otherCell.Initialize(cell.room);
-            }
-            passage.Initialize(otherCell, cell, direction.GetOpposite());
-        }
-
-        private void CreatePassageInSameRoom (BaseCell cell, BaseCell otherCell, CompassDirection direction)
-        {
-            CellPassage passage = Instantiate(ip.passagePrefab) as CellPassage;
-            passage.Initialize(cell, otherCell, direction);
-            passage = Instantiate(ip.passagePrefab) as CellPassage;
-            passage.Initialize(otherCell, cell, direction.GetOpposite());
-            if (cell.room != otherCell.room)
-            {
-                Room roomToMerge = otherCell.room;
-                cell.room.Merge(roomToMerge);
-                rooms.Remove(roomToMerge);
-                Destroy(roomToMerge);
-            }
-        }
-
-        private void CreateWall (BaseCell cell, BaseCell otherCell, CompassDirection direction)
-        {
-            CellWall wall = Instantiate(ip.wallPrefab) as CellWall;
-            wall.Initialize(cell, otherCell, direction);
-            if (otherCell != null)
-            {
-                wall = Instantiate(ip.wallPrefab) as CellWall;
-                wall.Initialize(otherCell, cell, direction.GetOpposite());
-            }
-        }
-
-        public IntVector2 RandomCoordinates
-        {
-            get
-            {
-                return new IntVector2(Random.Range(0, size.x), Random.Range(0, size.z));
-            }
-        }
-
-        public bool ContainsCoordinates (IntVector2 coordinate)
-        {
-            return coordinate.x >= 0 && coordinate.x < size.x && coordinate.z >= 0 && coordinate.z < size.z;
-        }
-
-        public BaseCell GetCell(IntVector2 coordinates)
-        {
-            return cells[coordinates.x, coordinates.z];
         }
 
     }
